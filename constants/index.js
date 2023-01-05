32aab7f0db58cf44a97890d9062174d54e4c5f35
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-05 18:09:33 -0500

    adding openzeplin

---
+++ constants/index.js
@@ -0,0 +1,10 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x42d805c28B6c6529bC379e315243ED8c58a1b7f0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "det-nft-app.vercel.app/api";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+// DET dev Contract address 0x12D02C54a34aDf4b61ac38AB7B2606e0DA2e769a
+// 2nd dev contract address to vercel det app 0x54b17810fc9C711a72305246E1A248Af34aB4fc2
+// Whitelist Contract Address: 0x89C76B782E0F0eBC0613106Ae484bC84d348AF95
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.17",
-// };
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
