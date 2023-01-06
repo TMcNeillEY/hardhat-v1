a80d4227cbb0062ccbc98e4ee59bc72c9e7c1f3f
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-05 19:08:32 -0500

    updating the working deploy.js and index.js

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x42d805c28B6c6529bC379e315243ED8c58a1b7f0";
+const WHITELIST_CONTRACT_ADDRESS = "0x3Ec92D0E5F1f157fBc93d9cA05Eb554BcCaF4C5e";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "det-nft-app.vercel.app/api";
 
@@ -8,3 +8,5 @@ module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 // DET dev Contract address 0x12D02C54a34aDf4b61ac38AB7B2606e0DA2e769a
 // 2nd dev contract address to vercel det app 0x54b17810fc9C711a72305246E1A248Af34aB4fc2
 // Whitelist Contract Address: 0x89C76B782E0F0eBC0613106Ae484bC84d348AF95
+//Whitelist Contract Address2: 0x3Ec92D0E5F1f157fBc93d9cA05Eb554BcCaF4C5e
+//DET contract address 2: Crypto Devs Contract Address: 0x36A6D45B1a7BD9982f28D617f8B857A788537c49
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,70 +1,28 @@
-// const { ethers } = require("hardhat");
-// require("dotenv").config({ path: ".env" });
-// const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
-
-// async function main() {
-//   // Address of the whitelist contract that you deployed in the previous module
-//   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
-//   // URL from where we can extract the metadata for a Crypto Dev NFT
-//   const metadataURL = METADATA_URL;
-//   /*
-//   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-//   so detDevsContract here is a factory for instances of our DetDevs contract.
-//   */
-//   const detDevsContract = await ethers.getContractFactory("DetDevs");
-
-//   // here we deploy the contract
-//   const deployedWhitelistContract = await whitelistContract.deploy(10);
-//   // 10 is the Maximum number of whitelisted addresses allowed
-//   // deploy the contract
-//   const deployedDetDevsContract = await detDevsContract.deploy(
-//     metadataURL,
-//     whitelistContract
-//   );
-  
-//   // // Wait for it to finish deploying
-//   // await deployedWhitelistContract.deployed();
-
-//   // print the address of the deployed contract
-// //   console.log(
-// //     "Whitelist Contract Address:",
-// //     deployedWhitelistContract.address
-// //   );
-// // }
-// console.log(
-//   "DET Devs Contract Address:",
-//   deployedDetDevsContract.address
-// );
-// }
-
-// // Call the main function and catch if there is any error
-// main()
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-//   });
-
 const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so whitelistContract here is a factory for instances of our Whitelist contract.
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
   */
-  const whitelistContract = await ethers.getContractFactory("Whitelist");
+  const detDevsContract = await ethers.getContractFactory("DetDevs");
 
-  // here we deploy the contract
-  const deployedWhitelistContract = await whitelistContract.deploy(10);
-  // 10 is the Maximum number of whitelisted addresses allowed
-  
-  // Wait for it to finish deploying
-  await deployedWhitelistContract.deployed();
+  // deploy the contract
+  const deployedDetDevsContract = await detDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
 
   // print the address of the deployed contract
   console.log(
-    "Whitelist Contract Address:",
-    deployedWhitelistContract.address
+    "DET Devs Contract Address:",
+    deployedDetDevsContract.address
   );
 }
 
@@ -75,3 +33,34 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+// const { ethers } = require("hardhat");
+
+// async function main() {
+//   /*
+//   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+//   so whitelistContract here is a factory for instances of our Whitelist contract.
+//   */
+//   const whitelistContract = await ethers.getContractFactory("Whitelist");
+
+//   // here we deploy the contract
+//   const deployedWhitelistContract = await whitelistContract.deploy(10);
+//   // 10 is the Maximum number of whitelisted addresses allowed
+  
+//   // Wait for it to finish deploying
+//   await deployedWhitelistContract.deployed();
+
+//   // print the address of the deployed contract
+//   console.log(
+//     "Whitelist Contract Address:",
+//     deployedWhitelistContract.address
+//   );
+// }
+
+// // Call the main function and catch if there is any error
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
