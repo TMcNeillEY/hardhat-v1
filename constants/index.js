777263f4d0d56d556697f4ed7f74dfa73a4ac582
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-05 20:29:39 -0500

    adding alchemy key url

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x3Ec92D0E5F1f157fBc93d9cA05Eb554BcCaF4C5e";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "det-nft-app.vercel.app/api";
+const METADATA_URL = "https://det-nft-app.vercel.app/api";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
